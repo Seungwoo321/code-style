27f4e7daf8c33b4dc03146381a70d15fdd18e369
Seungwoo321 <seungwoo321@gmail.com>
2025-05-05 18:43:50 +0900

    chore(eslint-plugin-standard-js): cleanup and add TS rule test

--- packages/eslint-plugin-standard-js/eslint.config.js
+++ packages/eslint-plugin-standard-js/eslint.config.js
@@ -7,8 +7,20 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs,ts,mts,jsx,tsx}'],
     ignores: ['lib/**/*'],
     extends: [
-      ...tseslint.configs.recommended,
+      ...tseslint.configs.strict,
+      ...tseslint.configs.stylistic,
       ...standard.configs.recommended
-    ]
+    ],
+    // config for custom rule test
+    rules: {
+      '@stylistic/max-len': ['error', {
+        code: 120,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true
+      }]
+    }
   }
 ])
